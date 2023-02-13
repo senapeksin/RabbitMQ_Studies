aff74eeba0dd8b8e07f61bfbc0ac261b77528f45
Senapeksinn <sena@endcozum.com>
2023-02-13 10:13:43 +0300

    Direct Exchange Davranışı

---
+++ RabbitMQ.DirectExchange.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// 1. Adım : Publisher'da ki exchane ile birebir aynı isim ve type'a sahip bir exchange tanımlanmalıdır.
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+// 2.Adım : Publisher tarafından routing key'de bulunan değerdeki kuyruğa gönderilen mesajları kendi oluşturduğumuz kuyruğa yönkendirerek tüketmemiz gerekmektedir.
+// Bunun için öncelikle bir kuyruk oluşturulmalıdır. 
+
+// Öncelikle bir kuyruk oluşturmamız lazım ve routing Key'de belirtilmiş olan kuyruk ismine karşılık gelen mesajların bizim oluşturacağımız bu kuyruğa yönlendirmemiz gerekecektir.
+// Ancak o zaman consumer ilgili mesajları tüketecektir.
+
+string queueName = channel.QueueDeclare().QueueName;  // Boş bırakıldığında RabbitMQ random bir isim verir. İsterseniz kendinizde kuyruk ismi girebilirsiniz.
+
+// Şimdi oluşturduğumuz bu queue'ya publisher uygulaması tarafından routing key' e gönderilen mesajları yönlendireceğim.
+// Bunu binding mekanizmasını kullanarak yapacağoz.
+
+// 3. Adım : 
+
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+
+consumer.Received += (sender, args) =>
+{
+    string message = Encoding.UTF8.GetString(args.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+Console.Read();
---
+++ RabbitMQ.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+// Bu exchange üzerinden herhangi bir mesajı gönderiyorsam yanni publish ediyorsam
+// bu exchange'in davranısı Direct olduğundan dolayı RabbitMQ routing key 'e bakacaktır.
+// Routing Key 'e karşılık gelen queue hangisi ise bu mesajı oraya gönderecektir.
+
+while (true)
+{
+    Console.WriteLine("Mesaj: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey: "direct-queue-example", body: byteMessage);  
+}
+ 
+Console.Read();
+
+
+// BİRDEN FAZLA KUYRUĞUN KULLANILDIĞI SENARYOLARDA İSTEDİĞİNİZ KUYRUĞA  HEDEF MESAJ GÖNDERMEK İSTİYORSANIZ DIRECT EXCHANGE DAVRANISINI KULLANABİLİRSİNİZ.
