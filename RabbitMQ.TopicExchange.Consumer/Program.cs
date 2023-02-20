086cab57c368a96d42cc2b3e420312e48eb0253d
Senapeksin <senapksnn@gmail.com>
2023-02-20 15:50:36 +0300

    Topic Exchange Davranışı tamamlandı

---
+++ RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Dinlenecek Topic formatını belirtiniz : ");
+string topicName = Console.ReadLine();
+
+// Kuyruk oluşturduk.
+string queueName = channel.QueueDeclare().QueueName; // RabbitMq 'nun random kuyruk ismini kullanmayı tercih ettik.
+
+// Oluşturduğumuz exchange ile kuyruğu bind etmiş olduk.
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topicName);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200); // her 20 ms'de bir bu işlemi gerçekleştirelim.
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba  {i}");
+    Console.Write("Mesajın gönderileceği Topic formatını belirtiniz : ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(exchange: "topic-exchange-example", routingKey: topic, body: message);
+}
+Console.Read();
