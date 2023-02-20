6d1f7d1d32c4265e7dd4412f523d2e739c5d7d3f
Senapeksin <senapksnn@gmail.com>
2023-02-20 15:14:55 +0300

    commit

--- RabbitMQ_FanoutExchange.Publisher/Program.cs
+++ RabbitMQ_FanoutExchange.Publisher/Program.cs
@@ -11,15 +11,11 @@ using IModel channel = connection.CreateModel();
 channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
 
 
-// Fanout Exchange: Herhangi bir kuyruğu ayırt etmeyeceğimizden dolayı routingKey boş bir değer verebiliriz.. Fanout Exchange, bu exchange' e bind olmuş kuyrukların hepsine isim gözetmeksizin mesajları ileteceği için routingKey boş olmalıdır.
-
-
-
+// Fanout Exchange Davranışı: Herhangi bir kuyruğu ayırt etmeyeceğimizden dolayı routingKey boş bir değer verebiliriz.. Fanout Exchange, bu exchange' e bind olmuş kuyrukların hepsine isim gözetmeksizin mesajları ileteceği için routingKey boş olmalıdır.
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
     byte[] message = Encoding.UTF8.GetBytes($"Merhaba  {i}");
     channel.BasicPublish(exchange: "fanout-exchange-example", routingKey: string.Empty, body: message);
-}
-
+}  
 Console.Read();
