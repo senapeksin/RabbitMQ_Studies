a396660ce9c98af8dcd1a7ae428f2502c59d8349
Senapeksin <senapksnn@gmail.com>
2023-02-24 16:30:45 +0300

    Header Exchange Davranışı

---
+++ RabbitMQ.HeaderExchange.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "header-exchange-example", type: ExchangeType.Headers);
+
+Console.Write("Dinlenecek Header value'sunu belirtiniz : ");
+string value = Console.ReadLine();
+
+// Kuyruk oluşturduk.
+string queueName = channel.QueueDeclare().QueueName; // RabbitMq 'nun random kuyruk ismini kullanmayı tercih ettik.
+
+// Oluşturduğumuz exchange ile kuyruğu bind etmiş olduk.
+channel.QueueBind(queue: queueName, exchange: "header-exchange-example", routingKey: string.Empty, new Dictionary<string, object>
+
+{
+    ["no"] = value
+} );
+
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.HeaderExchange.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://owqngobg:blIdj-tp5MdEGI55DGHK77IO6REX6hxx@stingray.rmq.cloudamqp.com/owqngobg");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "header-exchange-example", type: ExchangeType.Headers);
+
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.WriteLine("Lütfen Header value'sunnu giriniz.");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary <string,object>
+    {
+        ["no"] = value
+    };
+
+    channel.BasicPublish(exchange: "header-exchange-example", routingKey: string.Empty, body: message, basicProperties: basicProperties);
+
+} 
+
+Console.Read();
